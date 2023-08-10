247e0719dfac650f183654152b9fc7a8f52e41ff
Tony Kim <wukelong123@gmail.com>
2023-08-10 10:02:49 +0800

    disable firewall

--- lama_cleaner/app/config/webpackDevServer.config.js
+++ lama_cleaner/app/config/webpackDevServer.config.js
@@ -15,7 +15,7 @@ const sockPort = process.env.WDS_SOCKET_PORT;
 
 module.exports = function (proxy, allowedHost) {
   const disableFirewall =
-    !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true';
+    !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true' || true;
   return {
     // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
     // websites from potentially accessing local content through DNS rebinding:
